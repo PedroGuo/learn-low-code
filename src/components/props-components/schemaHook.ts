0fd7e3eb4880af473f5381761d92b8c4cad63bc0
guoyan <guoyan@exexm.com>
2022-03-23 13:36:07 +0800

    fix: 修改不能双向绑定bug

--- src/components/props-components/schemaHook.ts
+++ src/components/props-components/schemaHook.ts
@@ -1,7 +1,7 @@
 import { ref, watch, toRefs } from "vue";
 export default function (props: any, emit: any) {
   const { value, label, options } = toRefs(props);
-  const eValue = ref(value);
+  const eValue = ref('');
 
   watch(eValue, (value) => {
     emit("update:modelValue", value);
